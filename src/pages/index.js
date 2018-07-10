8bf3bc75f9ce545561bca46ba3b07f4b548d44e2
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-07-10 12:07:54 +0100

    Adding semantic-ui-react (no CSS)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import Button from '../components/Button'
+
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <Button>Hello Button</Button>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
