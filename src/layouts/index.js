0228768bcd28fef902ee64bdd0ec1fe59b56ca9d
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-07-10 12:45:20 +0100

    Adding rebass

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,27 +5,37 @@ import Helmet from 'react-helmet'
 import Header from '../components/header'
 import './index.css'
 
+import { Provider } from "rebass"
+import { injectGlobal } from 'styled-components'
+
+injectGlobal`
+  * { box-sizing: border-box; }
+  body { margin: 0; }
+`
+
 const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+  <Provider>
+    <div>
+      <Helmet
+        title={data.site.siteMetadata.title}
+        meta={[
+          { name: 'description', content: 'Sample' },
+          { name: 'keywords', content: 'sample, something' },
+        ]}
+      />
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+          padding: '0px 1.0875rem 1.45rem',
+          paddingTop: 0,
+        }}
+      >
+        {children()}
+      </div>
     </div>
-  </div>
+  </Provider>
 )
 
 Layout.propTypes = {
