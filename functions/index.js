98fb2bb7c591ae601d85ee31e6514d94db81dbfb
Richard Moss <richard@englishtutorslive.com>
2018-07-11 16:27:10 +0100

    Add basic function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,66 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+
+admin.initializeApp(functions.config().firebase);
+
+
+
+exports.sendEmail = functions.database
+                              .ref('/contactSubmissions/{name}')
+                              .onWrite((snap, context) => {
+                              console.log('event', snap.after.val())
+                              })
+
+
+                              // exports.helloWorld = functions.https.onRequest((request, response) => {
+                              //  response.send("Hello from Firebase!");
+                              // })
+
+// exports.stripeCharge = functions.database
+//                                 .ref('/donations/{userId}/{paymentId}/payload')
+//                                 .onWrite((snap, context) => {
+//
+//   console.log('EVENT', snap.after.val())
+//   const payment = snap.after.val()
+//   const userId = context.params.userId;
+//   const paymentId = context.params.paymentId;
+//
+//   // return admin.database()
+//   //      .ref(`/donations/{userId}/{paymentId}/charge`)
+//   //      .set({event: amount})
+//   //
+//   // console.log('after write')
+//
+//   // checks if payment exists or if it has already been charged
+//   if (!payment || payment.charge) return;
+//
+//   return admin.database()
+//               .ref(`/users/${userId}`)
+//               .once('value')
+//               .then(snapshot => {
+//                   return snapshot.val();
+//                })
+//                .then(customer => {
+//
+//                  const amount = payment.amount;
+//                  const idempotency_key = paymentId;  // prevent duplicate charges
+//                  const source = payment.token.id;
+//                  const currency = 'gbp';
+//                  const charge = {amount, currency, source};
+//
+//
+//                  return stripe.charges.create(charge, { idempotency_key });
+//
+//                })
+//
+//                .then(charge => {
+//                    admin.database()
+//                         .ref(`/payments/${userId}/${paymentId}/charge`)
+//                         .set(charge)
+//                   })
+//   //
+//
+// });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
