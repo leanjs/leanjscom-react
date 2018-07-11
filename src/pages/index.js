b51a04e2d227b588c72dd534a71f719241721d8c
Richard Moss <richard@englishtutorslive.com>
2018-07-11 13:59:10 +0100

    Remove unused firebase export and import component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import RebassTestCard from '../components/RebassTestCard'
+import ContactForm from '../components/ContactForm'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
+    <h1>Hi asdas</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <RebassTestCard />
     <Link to="/page-2/">Go to page 2</Link>
+    <ContactForm />
   </div>
 )
 
