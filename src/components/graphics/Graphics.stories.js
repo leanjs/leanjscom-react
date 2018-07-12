389afa9168c8634b79aaa11449612317361133d8
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-07-12 11:02:03 +0100

    Adding iteration graphic

---
+++ src/components/graphics/Graphics.stories.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Iteration from './Iteration'
+
+storiesOf('Graphics', module)
+  .add('Iteration', () => <Iteration />)
