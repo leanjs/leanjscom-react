e41fdb12c924e2518039da7887a544279fe87e87
Richard Moss <richard@englishtutorslive.com>
2018-07-12 18:03:47 +0100

    Update dependenies for Grid and Nav and update header

--- src/components/navigation/Menu.js
+++ src/components/navigation/Menu.js
@@ -1,20 +1,31 @@
 import React from 'react'
 import SideMenu from 'react-burger-menu'
+import MenuToggleButton from './MenuToggleButton'
+import styled from 'styled-components'
+import Grid from '../layout/Grid'
 
 // import FloatingMenuBtn from '../../../FloatingMenuBtn'
+const Background = styled.div`
+  background-color: black;
+`
+
 
 const Menu = ({ isOpen, children, pageWrapId, width, toggleMenu}) => (
-  <div>
-    {/* { width === LARGE ? '' :
-      <FloatingMenuBtn toggleMenu={ toggleMenu } />
-    } */}
-    <SideMenu.slide
-       isOpen={ isOpen }
-       pageWrapId={ pageWrapId || 'page-wrap' }
-    >
-      { children }
-    </SideMenu.slide>
-  </div>
+  <Grid>
+    <Background>
+      {/* { width === LARGE ? '' :
+        <FloatingMenuBtn toggleMenu={ toggleMenu } />
+      } */}
+      <MenuToggleButton />
+      <SideMenu.slide
+         isOpen={ isOpen }
+         pageWrapId={ pageWrapId || 'page-wrap' }
+      >
+        { children }
+      </SideMenu.slide>
+    </Background>
+  </Grid>
+
 )
 
 export default Menu
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import ContactForm from '../components/ContactForm'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi asdas</h1>
+    <h1>Hi asdaasdasdass</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
