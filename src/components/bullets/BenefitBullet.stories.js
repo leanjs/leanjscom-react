398e4d79a178a2cc28be42783de8f27bcc28a4b5
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-07-12 10:43:58 +0100

    Adding more benefit bullets

--- src/components/bullets/BenefitBullet.stories.js
+++ src/components/bullets/BenefitBullet.stories.js
@@ -2,6 +2,13 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import BenefitBullet from './BenefitBullet'
 
-storiesOf('BenefitBullet', module).add('Quality', () => (
-  <BenefitBullet image="quality" />
-))
+storiesOf('BenefitBullet', module)
+	.add('Quality', () => (
+  		<BenefitBullet image="quality" />
+	))
+	.add('Cost effective', () => (
+  		<BenefitBullet image="costeffective" />
+	))
+	.add('Quick', () => (
+  		<BenefitBullet image="quick" />
+	))
