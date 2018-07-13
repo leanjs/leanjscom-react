9e46e2fb6de1fac84bbc4c9aeb6963e2878cf89b
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-07-13 17:00:14 +0100

    Adding options for large logo and text component

--- src/components/bulletedsections/LargeLogoAndPoints.js
+++ src/components/bulletedsections/LargeLogoAndPoints.js
@@ -13,11 +13,15 @@ const Columns = styled.div`
     margin-top: 0;
     margin-bottom: 0;
   }
+
+  li {
+    color: ${props => props.color || 'black'};
+  }
 `
 
-const LargeLogoAndPoints = props => (
+const LargeLogoAndPoints = ({ color, ...props }) => (
   <Line verticalCenter>
-    <Columns>
+    <Columns color={color}>
       <CaseStudyBullet className="case-study-bullet" image={props.image} />
       <ul className="points">{props.points.map(point => <li>{point}</li>)}</ul>
     </Columns>
--- src/components/bulletedsections/bulletedsections.stories.js
+++ src/components/bulletedsections/bulletedsections.stories.js
@@ -3,9 +3,18 @@ import { storiesOf } from '@storybook/react'
 
 import LargeLogoAndPoints from './LargeLogoAndPoints'
 
-storiesOf('Bulleted sections', module).add('Large logo and points', () => (
-  <LargeLogoAndPoints
-    image="abinbev"
-    points={['Rapid app design and build', '6 week turnaround']}
-  />
-))
+storiesOf('Bulleted sections', module)
+  .add('Large logo and points', () => (
+    <LargeLogoAndPoints
+      image="abinbev"
+      points={['Rapid app design and build', '6 week turnaround']}
+      color="white"
+    />
+  ))
+  .add('Large logo and points (dark text)', () => (
+    <LargeLogoAndPoints
+      image="abinbev"
+      points={['Rapid app design and build', '6 week turnaround']}
+      color="black"
+    />
+  ))
