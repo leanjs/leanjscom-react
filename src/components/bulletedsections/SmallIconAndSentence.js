4dc31f77e2e07635509b7b7980617c2b663f12df
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-07-13 17:16:48 +0100

    Adding small icon and sentence

---
+++ src/components/bulletedsections/SmallIconAndSentence.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Columns = styled.div`
+  display: flex;
+  align-items: center;
+  color: white;
+
+  div.small-icon-sentence {
+    padding-left: 15px;
+  }
+`
+
+const SmallIconAndSentence = ({ icon, ...props }) => (
+  <Columns>
+    {icon}
+    <div className="small-icon-sentence">{props.sentence}</div>
+  </Columns>
+)
+
+export default SmallIconAndSentence
--- src/components/bulletedsections/bulletedsections.stories.js
+++ src/components/bulletedsections/bulletedsections.stories.js
@@ -1,21 +1,29 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import LargeLogoAndPoints from './LargeLogoAndPoints'
+import SmallIconAndSentence from './SmallIconAndSentence'
 import theme from '../../layouts/rebass-theme'
 
+import ValueBullet from '../bullets/ValueBullet'
+
 storiesOf('Bulleted sections', module)
-  .add('Large logo and points', () => (
-    <LargeLogoAndPoints
+  .add('Large icon and points', () => (
+    <LargeIconAndPoints
       image="abinbev"
       points={['Rapid app design and build', '6 week turnaround']}
       color="white"
     />
   ))
-  .add('Large logo and points (dark text)', () => (
-    <LargeLogoAndPoints
+  .add('Large icon and points (dark text)', () => (
+    <LargeIconAndPoints
       image="abinbev"
       points={['Rapid app design and build', '6 week turnaround']}
       color={theme.colors.darkgrey}
+    />
+  ))
+  .add('Small icon and sentence', () => (
+    <SmallIconAndSentence
+      icon={<ValueBullet image="learning" />}
+      sentence="Be unafraid to learn from everyone and believe in teaching what we know"
     />
   ))
