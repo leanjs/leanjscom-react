552af3ccbb3fc6eeaeee7ada742c6b6aacae7ed9
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-03 12:30:04 +0100

    Adding checkbox component

--- src/components/layout/Checkbox.js
+++ src/components/layout/Checkbox.js
@@ -1,19 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
+import { WHITE } from '../../config/styles'
+
+const defaultSize = 3
 
 const StyledCheckbox = styled.div`
-  input {
-    height: 3em;
-    width: 3em;
-    background-color: white;
+  .displayCheckbox {
+    background: ${WHITE};
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    .displayCheckboxInner {
+      background: transparent
+        url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjAuMjg1IDJsLTExLjI4NSAxMS41NjctNS4yODYtNS4wMTEtMy43MTQgMy43MTYgOSA4LjcyOCAxNS0xNS4yODV6Ii8+PC9zdmc+)
+        center/contain no-repeat scroll;
+      height: 90%;
+      width: 90%;
+      margin: auto;
+
+      display: ${props => (props.checked ? 'block' : 'none')};
+    }
   }
+  height: ${props => (props.size ? props.size : defaultSize)}em;
+  width: ${props => (props.size ? props.size : defaultSize)}em;
   display: inline-block;
   vertical-align: middle;
 `
 
 const Checkbox = props => (
-  <StyledCheckbox>
-    <input type="checkbox" {...props} />
+  <StyledCheckbox {...props}>
+    <div className="displayCheckbox">
+      <div className="displayCheckboxInner" />
+    </div>
+    <input type="hidden" value={props.checked ? '1' : '0'} />
   </StyledCheckbox>
 )
 
