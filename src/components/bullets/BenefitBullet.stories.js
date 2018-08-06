9f5538604c60b3e513c55fdac6edf7092647d2ec
Richard Moss <richard@englishtutorslive.com>
2018-08-06 19:44:24 +0200

    Update package.json

--- src/components/bullets/BenefitBullet.stories.js
+++ src/components/bullets/BenefitBullet.stories.js
@@ -3,12 +3,6 @@ import { storiesOf } from '@storybook/react'
 import BenefitBullet from './BenefitBullet'
 
 storiesOf('BenefitBullet', module)
-	.add('Quality', () => (
-  		<BenefitBullet image="quality" />
-	))
-	.add('Cost effective', () => (
-  		<BenefitBullet image="costeffective" />
-	))
-	.add('Quick', () => (
-  		<BenefitBullet image="quick" />
-	))
+  .add('Quality', () => <BenefitBullet image="quality" />)
+  .add('Cost effective', () => <BenefitBullet image="costeffective" />)
+  .add('Quick', () => <BenefitBullet image="quick" />)
--- src/components/graphics/Iteration.stories.js
+++ src/components/graphics/Iteration.stories.js
@@ -2,5 +2,4 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Iteration from './Iteration'
 
-storiesOf('Iteration graphic', module)
-  .add('Standard', () => <Iteration />)
+storiesOf('Iteration graphic', module).add('Standard', () => <Iteration />)
