4407648be346f19fb6318423e40ec483aecdebea
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-07 21:01:01 +0100

    Tidying concentric circles and adding stories to illustrate

--- src/components/decoration/ConcentricCircles.js
+++ src/components/decoration/ConcentricCircles.js
@@ -8,12 +8,14 @@ const defaultSize = 360
 const defaultColor = DARKGREY
 
 const ConcentricCircles = props => {
-  const numCircles = props.numCircles ? props.numCircles : defaultNumCircles
-  const outerSize = props.size ? props.size : defaultSize
-  const color = props.color ? props.color : DARKGREY
+  const {
+    numCircles = defaultNumCircles,
+    color = DARKGREY,
+    nestingLevel = 0,
+    size: outerSize = defaultSize,
+  } = props
 
   const stepSize = outerSize / numCircles
-  const nestingLevel = props.nestingLevel ? props.nestingLevel : 0
   const thisCircleSize = outerSize - stepSize * nestingLevel
 
   // thisCirlceSize should be exactly 0 after the numCircles circles have been added.
--- src/components/decoration/ConcentricCircles.stories.js
+++ src/components/decoration/ConcentricCircles.stories.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import ConcentricCircles from './ConcentricCircles'
 
-storiesOf('ConcentricCircles', module).add('ConcentricCircles', () => (
-  <ConcentricCircles />
-))
+storiesOf('ConcentricCircles', module)
+  .add('Basic', () => <ConcentricCircles />)
+  .add('Different sizes', () => <ConcentricCircles size={520} />)
+  .add('Different numbers of circles', () => (
+    <ConcentricCircles numCircles={30} size={520} />
+  ))
+  .add('Different colours', () => <ConcentricCircles color="blue" size={520} />)
