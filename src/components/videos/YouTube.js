4f6339334742bec83e159b900fcdfacfcb7db72d
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-10 17:56:07 +0100

    Adding helpful comment about the youtube embed styles

--- src/components/videos/YouTube.js
+++ src/components/videos/YouTube.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
+// Responsive width youtube embed, from
+// https://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
 const YouTubeInner = styled.div`
   position: relative;
   padding-bottom: 56.25%; /* 16:9 */
