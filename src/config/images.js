20dd3d6f2404202f240a9af0903d86c8a74ef822
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-13 13:45:50 +0100

    Remove auth token from london bootcamp image

--- src/config/images.js
+++ src/config/images.js
@@ -1,2 +1,2 @@
 export const LONDON_BOOTCAMP =
-  'https://firebasestorage.googleapis.com/v0/b/leanjscom-web.appspot.com/o/london-bootcamp.png?alt=media&token=a6f92226-24a6-4dd3-8b8c-b2c0be65290a'
+  'https://firebasestorage.googleapis.com/v0/b/leanjscom-web.appspot.com/o/london-bootcamp.png?alt=media'
