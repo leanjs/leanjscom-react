e8931fab501bd3f29cbe8ca9429cda034b23a28f
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-17 19:28:37 +0100

    Improving spacing at top and bottom of sections

--- src/components/layout/Section.js
+++ src/components/layout/Section.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
 import { SCREEN_SM_MIN, SCREEN_XS_MAX } from '../utils'
+
 import {
   EXTRADARKGREY,
   WHITE,
   FONT_FAMILY,
-  SPACING_LARGE,
+  SPACING_EXTRALARGE,
 } from '../../config/styles'
 
 const Section = styled.section`
   ${FONT_FAMILY}
-  padding-top: ${props => (props.top ? '0' : SPACING_LARGE)};
-  padding-bottom: ${SPACING_LARGE};
+  padding-top: ${props => (props.top ? '0' : SPACING_EXTRALARGE)};
+  padding-bottom: ${SPACING_EXTRALARGE};
   background-color: ${props => props.dark && EXTRADARKGREY};
   color: ${props => props.dark && WHITE};
 `
