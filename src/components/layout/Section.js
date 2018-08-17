6928d5c76590017a8761fb4e17428fac212ded37
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-17 19:31:39 +0100

    Reducing section padding back down on mobiles

--- src/components/layout/Section.js
+++ src/components/layout/Section.js
@@ -6,6 +6,7 @@ import {
   EXTRADARKGREY,
   WHITE,
   FONT_FAMILY,
+  SPACING_LARGE,
   SPACING_EXTRALARGE,
 } from '../../config/styles'
 
@@ -13,6 +14,10 @@ const Section = styled.section`
   ${FONT_FAMILY}
   padding-top: ${props => (props.top ? '0' : SPACING_EXTRALARGE)};
   padding-bottom: ${SPACING_EXTRALARGE};
+  @media (max-width:${SCREEN_XS_MAX}) {
+  	padding-top:${SPACING_LARGE};
+    padding-bottom:${SPACING_LARGE};
+  }
   background-color: ${props => props.dark && EXTRADARKGREY};
   color: ${props => props.dark && WHITE};
 `
