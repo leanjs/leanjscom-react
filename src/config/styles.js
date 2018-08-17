bc27ca6fe3215c124e5bf65d4b584263ab49413f
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-17 12:45:12 +0100

    Add medium-light-grey colour as used in the 'border' around some grid sections

--- src/config/styles.js
+++ src/config/styles.js
@@ -4,6 +4,7 @@ export const BOX_SHADOW =
   'box-shadow: 0 -2px 24px 0 rgba(0, 0, 0, 0.24), 0 2px 24px 0 rgba(0, 0, 0, 0.12);'
 
 export const LIGHTGREY = '#d8d8d8'
+export const MEDIUMLIGHTGREY = '#979797'
 export const MEDIUMGREY = '#9b9b9b'
 export const DARKGREY = '#4a4a4a'
 export const EXTRADARKGREY = '#2b2b2b'
