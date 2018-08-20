89ad92b8c81d914ab80d71f58a282244e1f7735f
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-20 17:06:04 +0100

    Adding story back for line verticalCenter

--- src/components/sections/sections.stories.js
+++ src/components/sections/sections.stories.js
@@ -4,4 +4,12 @@ import Line from './Line'
 
 storiesOf('Sections', module)
   .add('Standard', () => <Line>Section contents</Line>)
+  .add('Standard (vertical center)', () => (
+    <Line verticalCenter>
+      <p>Section contents para</p>
+      <p>Section contents para</p>
+      <p>Section contents para</p>
+      <p>Section contents para</p>
+    </Line>
+  ))
   .add('Wide', () => <Line wide>Section contents</Line>)
