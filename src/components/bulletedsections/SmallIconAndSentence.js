c2e6a8b47e79eac80c575843cfde32fdd1d97239
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-20 15:50:26 +0100

    Improving vertical spacing around core values

--- src/components/bulletedsections/SmallIconAndSentence.js
+++ src/components/bulletedsections/SmallIconAndSentence.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import { SPACING_STANDARD } from '../../config/styles'
+import { SPACING_STANDARD, SPACING_LARGE } from '../../config/styles'
+import { SCREEN_SM_MAX } from '../utils'
 
 const Table = styled.div`
   display: table;
@@ -11,16 +12,29 @@ const Row = styled.div`
   > div {
   }
 `
+const getLeftPadding = isWideScreen => props =>
+  `padding-left: ${
+    props.firstTable || (props.firstCell && !isWideScreen)
+      ? `0`
+      : `${isWideScreen ? SPACING_LARGE : SPACING_STANDARD}`
+  }`
+
 const Cell = styled.div`
   display: table-cell;
   vertical-align: middle;
-  padding-left: ${props => (props.first ? `0` : SPACING_STANDARD)};
+  padding: ${SPACING_STANDARD};
+  ${getLeftPadding(true)};
+  @media (max-width: ${SCREEN_SM_MAX}) {
+    ${getLeftPadding(false)};
+  }
 `
 
 const SmallIconAndSentence = props => (
   <Table>
     <Row>
-      <Cell first={props.first}>{props.icon}</Cell>
+      <Cell firstCell firstTable={props.first}>
+        {props.icon}
+      </Cell>
       <Cell>{props.sentence}</Cell>
     </Row>
   </Table>
