d48901ba13bd056c8be64b15e2d7f68084c156ae
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-21 15:05:02 +0100

    Adding grids to building-solutions page

--- src/pages/building-solutions.js
+++ src/pages/building-solutions.js
@@ -62,169 +62,177 @@ const BuildingSolutions = () => (
     </Header>
 
     <Section>
-      <Row>
-        <Col>
-          <a name="workingWithYou" />
-          <H2>Working in collaboration with YOU</H2>
-        </Col>
-      </Row>
-      <Row>
-        <Col center md={6}>
-          <P>
-            We know how painful digital projects can be. Our team have many
-            years experience in digital and have developed a process that works
-            with you - from beginning to end.
-          </P>
-        </Col>
-        <Col center md={6}>
-          <Image src={'https://picsum.photos/300/200/?random'} />
-        </Col>
-      </Row>
-      <Row>
-        <Col md={4}>
-          <SmallIconAndSentence
-            icon={<BenefitBullet image="chat" />}
-            sentence="Be unafraid to learn from everyone and believe in teaching what we know"
-          />
-        </Col>
-        <Col md={4}>
-          <SmallIconAndSentence
-            icon={<BenefitBullet image="gift" />}
-            sentence="Explain all possibilities with you and collaborate to prioritise the best solutions"
-          />
-        </Col>
-        <Col md={4}>
-          <SmallIconAndSentence
-            icon={<BenefitBullet image="calendar" />}
-            sentence="Bi-weekly reviews throughout the design and build process"
-          />
-        </Col>
-      </Row>
-      <Row>
-        <Col center>
-          <Blockquote2>
-            We never thought it possible! We only had a couple of months until
-            Christmas but LeanJS sorted us out. They were highly professional
-            yet friendly and very effective.
-          </Blockquote2>
-        </Col>
-      </Row>
-      <Row>
-        <Col center md={6}>
-          <Mugshot
-            image={MUGSHOT_JAMES_NORTON}
-            name="Bill Pierce"
-            jobTitle="Editor, AGNI"
-            company="Boston University"
-            color={DARKGREY}
-          />
-        </Col>
-        <Col center md={6}>
-          <LinkButton dark hasArrows>
-            Read our case studies
-          </LinkButton>
-        </Col>
-      </Row>
+      <Grid>
+        <Row>
+          <Col>
+            <a name="workingWithYou" />
+            <H2>Working in collaboration with YOU</H2>
+          </Col>
+        </Row>
+        <Row>
+          <Col center md={6}>
+            <P>
+              We know how painful digital projects can be. Our team have many
+              years experience in digital and have developed a process that
+              works with you - from beginning to end.
+            </P>
+          </Col>
+          <Col center md={6}>
+            <Image src={'https://picsum.photos/300/200/?random'} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <SmallIconAndSentence
+              icon={<BenefitBullet image="chat" />}
+              sentence="Be unafraid to learn from everyone and believe in teaching what we know"
+            />
+          </Col>
+          <Col md={4}>
+            <SmallIconAndSentence
+              icon={<BenefitBullet image="gift" />}
+              sentence="Explain all possibilities with you and collaborate to prioritise the best solutions"
+            />
+          </Col>
+          <Col md={4}>
+            <SmallIconAndSentence
+              icon={<BenefitBullet image="calendar" />}
+              sentence="Bi-weekly reviews throughout the design and build process"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col center>
+            <Blockquote2>
+              We never thought it possible! We only had a couple of months until
+              Christmas but LeanJS sorted us out. They were highly professional
+              yet friendly and very effective.
+            </Blockquote2>
+          </Col>
+        </Row>
+        <Row>
+          <Col center md={6}>
+            <Mugshot
+              image={MUGSHOT_JAMES_NORTON}
+              name="Bill Pierce"
+              jobTitle="Editor, AGNI"
+              company="Boston University"
+              color={DARKGREY}
+            />
+          </Col>
+          <Col center md={6}>
+            <LinkButton dark hasArrows>
+              Read our case studies
+            </LinkButton>
+          </Col>
+        </Row>
+      </Grid>
     </Section>
 
     <Section dark>
-      <Row>
-        <Col>
-          <a name="projectHandovers" />
-          <H2>Slick handovers with expert tech training</H2>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <P>
-            Anyone who’s completed a digital project knows how important a good
-            handover is.
-          </P>
-          <P>
-            That’s why we don’t just build but train too. This means that when
-            it comes to a product handover, we’re there for you and your team.
-          </P>
-          <P>We can provide training for:</P>
-          <Ul>
-            <Li>
-              The ReactJS ecosystem
-              <Li>Working with GraphQL</Li>
-              <Li>Lean/Agile processes</Li>
-              <Li>Product & UX design</Li>
-            </Li>
-          </Ul>
-        </Col>
-      </Row>
-      <Row>
-        <Col center>
-          <Image src={'https://picsum.photos/300/200/?random'} />
-        </Col>
-      </Row>
-      <Row>
-        <Col center>
-          <LinkButton hasArrows>Find out more about our training</LinkButton>
-        </Col>
-      </Row>
+      <Grid>
+        <Row>
+          <Col>
+            <a name="projectHandovers" />
+            <H2>Slick handovers with expert tech training</H2>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <P>
+              Anyone who’s completed a digital project knows how important a
+              good handover is.
+            </P>
+            <P>
+              That’s why we don’t just build but train too. This means that when
+              it comes to a product handover, we’re there for you and your team.
+            </P>
+            <P>We can provide training for:</P>
+            <Ul>
+              <Li>
+                The ReactJS ecosystem
+                <Li>Working with GraphQL</Li>
+                <Li>Lean/Agile processes</Li>
+                <Li>Product & UX design</Li>
+              </Li>
+            </Ul>
+          </Col>
+        </Row>
+        <Row>
+          <Col center>
+            <Image src={'https://picsum.photos/300/200/?random'} />
+          </Col>
+        </Row>
+        <Row>
+          <Col center>
+            <LinkButton hasArrows>Find out more about our training</LinkButton>
+          </Col>
+        </Row>
+      </Grid>
     </Section>
-    <Row>
-      <Col>
-        <a name="support" />
-        <H2>What happens next? How we support you…</H2>
-      </Col>
-    </Row>
-    <Row>
-      <Col center>
-        <Image src={'https://picsum.photos/500/200/?random'} />
-      </Col>
-    </Row>
-    <Row>
-      <Col center>
-        <P>
-          Building a website or app is scary - we get it. But sometimes the real
-          challenges start once the project is launched and live.
-        </P>
-      </Col>
-      <Col center>
-        <P>
-          That’s why we promise that you wont be left in the lurch, we’ll be
-          there to help you as much as you require. We wont dump you in it at
-          the end of the project!
-        </P>
-      </Col>
-    </Row>
-    <Row>
-      <Col md={4}>
-        <SmallIconAndSentence
-          icon={<BenefitBullet image="tools" />}
-          sentence="Tailored, bespoke support packages"
-        />
-      </Col>
-      <Col md={4}>
-        <SmallIconAndSentence
-          icon={<BenefitBullet image="code" />}
-          sentence="Proven, open sourced tech-stack under the hood that gives your team total flexibility"
-        />
-      </Col>
-      <Col md={4}>
-        <SmallIconAndSentence
-          icon={<BenefitBullet image="expand" />}
-          sentence="Robust, scalable technology that will live far into the future"
-        />
-      </Col>
-    </Row>
-    <Row>
-      <Col center md={6}>
-        <LinkButton dark hasArrows to="/case-studies">
-          Read our case studies
-        </LinkButton>
-      </Col>
-      <Col center md={6}>
-        <LinkButton hasArrows to="/development-services">
-          Development services
-        </LinkButton>
-      </Col>
-    </Row>
-    <Section />
+
+    <Section>
+      <Grid>
+        <Row>
+          <Col>
+            <a name="support" />
+            <H2>What happens next? How we support you…</H2>
+          </Col>
+        </Row>
+        <Row>
+          <Col center>
+            <Image src={'https://picsum.photos/500/200/?random'} />
+          </Col>
+        </Row>
+        <Row>
+          <Col center>
+            <P>
+              Building a website or app is scary - we get it. But sometimes the
+              real challenges start once the project is launched and live.
+            </P>
+          </Col>
+          <Col center>
+            <P>
+              That’s why we promise that you wont be left in the lurch, we’ll be
+              there to help you as much as you require. We wont dump you in it
+              at the end of the project!
+            </P>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={4}>
+            <SmallIconAndSentence
+              icon={<BenefitBullet image="tools" />}
+              sentence="Tailored, bespoke support packages"
+            />
+          </Col>
+          <Col md={4}>
+            <SmallIconAndSentence
+              icon={<BenefitBullet image="code" />}
+              sentence="Proven, open sourced tech-stack under the hood that gives your team total flexibility"
+            />
+          </Col>
+          <Col md={4}>
+            <SmallIconAndSentence
+              icon={<BenefitBullet image="expand" />}
+              sentence="Robust, scalable technology that will live far into the future"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col center md={6}>
+            <LinkButton dark hasArrows to="/case-studies">
+              Read our case studies
+            </LinkButton>
+          </Col>
+          <Col center md={6}>
+            <LinkButton hasArrows to="/development-services">
+              Development services
+            </LinkButton>
+          </Col>
+        </Row>
+      </Grid>
+    </Section>
   </React.Fragment>
 )
 
