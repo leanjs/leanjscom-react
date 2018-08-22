80eec6fd4e330fa72fb41c613746b79e15a80092
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-22 11:34:28 +0100

    Splitting icons into separate file to make the 'icons' file easier to open and manage

---
+++ src/config/icons/quality.js
@@ -0,0 +1,2 @@
+export const QUALITY =
+  'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBzdGFuZGFsb25lPSJubyI/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjI2MCIgaGVpZ2h0PSIyNDUiPgo8cGF0aCBkPSJtNTUsMjM3IDc0LTIyOCA3NCwyMjhMOSw5NmgyNDAiLz4KPC9zdmc+'
