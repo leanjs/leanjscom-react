28b1bf15039f99d3ce6be8126c5d8a9471ec499b
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-22 09:31:35 +0100

    Increasing size of LeanJS logo in header

--- src/components/logos/LeanJS.stories.js
+++ src/components/logos/LeanJS.stories.js
@@ -2,6 +2,4 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { LeanJSLogo } from './LeanJS'
 
-storiesOf('LeanJS Logo', module)
-  .add('Standard logo', () => <LeanJSLogo />)
-  .add('Different colours', () => <LeanJSLogo color="#f8f5e3" />)
+storiesOf('LeanJS Logo', module).add('Standard logo', () => <LeanJSLogo />)
