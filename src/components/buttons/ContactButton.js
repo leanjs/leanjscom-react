50d1fcba5bc05020d90fa6124a7660a9a14c0a91
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-22 12:56:17 +0100

    Making ContactButton actually link to the contact form

--- src/components/buttons/ContactButton.js
+++ src/components/buttons/ContactButton.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { LinkIconButton } from './IconButton'
 import { CHAT } from '../../config/icons'
 
-const ContactButton = () => <LinkIconButton image={CHAT} text="Contact" />
+const ContactButton = () => (
+  <LinkIconButton image={CHAT} text="Contact" to="#contact" />
+)
 
 export default ContactButton
