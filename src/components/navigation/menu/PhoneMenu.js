0543c62310aab9dfafae54403954d4ee1a2a696a
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-24 12:54:37 +0100

    Smooth-scrolling contact-us button in mobile site

--- src/components/navigation/menu/PhoneMenu.js
+++ src/components/navigation/menu/PhoneMenu.js
@@ -1,6 +1,7 @@
 import { slide as Menu } from 'react-burger-menu'
 import styled from 'styled-components'
 import Link from '../Link'
+import { Link as LinkScroll } from 'react-scroll'
 import React from 'react'
 import MenuData from './Menu.json'
 import { LinkedLeanJSLogo } from '../../logos/LeanJS'
@@ -8,7 +9,11 @@ import ContactButton from '../../buttons/ContactButton'
 import './PhoneMenu.css'
 import { SPACING_SMALL } from '../../../config/styles'
 
-const Item = ({ children, ...props }) => <Link {...props}>{children}</Link>
+const Item = ({ children, ...rest }) => {
+  const LinkType = scroll ? LinkScroll : Link
+  const props = scroll ? { smooth: true, duration: 500, ...rest } : rest
+  return <LinkType {...props}>{children}</LinkType>
+}
 
 const PhoneMenuItem = styled(Item)`
   display: block;
@@ -43,7 +48,8 @@ class PhoneMenu extends React.Component {
         <PhoneMenuItem
           onClick={closeMenu}
           key="phonemenu-contact"
-          to="#contact"
+          to="contact"
+          scroll
         >
           Contact us
         </PhoneMenuItem>
