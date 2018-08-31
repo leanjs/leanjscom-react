ad8202ef2c3ced8e0d061ddde83a680f4b7adb41
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-08-31 11:55:54 +0100

    Fixing error by removing TEXT_SIZE from the P component (TEXT_SIZE does not exist as a constant; it is set site-wide in the main CSS file)

--- src/components/text/P.js
+++ src/components/text/P.js
@@ -2,18 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 import {
   FONT_FAMILY,
-  TEXT_SIZE,
   FONT_SIZE_STANDARD,
   SPACING_STANDARD,
 } from '../../config/styles'
 
 export default styled.p`
-  ${TEXT_SIZE}
   ${FONT_FAMILY}
   text-align: ${props => props.align || 'left'};
   margin: 0;
   padding-bottom: ${SPACING_STANDARD};
-  > a {
-    ${TEXT_SIZE}
-  }
 `
