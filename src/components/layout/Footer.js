da183802257164c9ced4f832c4a23c1e5569b551
Will Voelcker <wvoelcker@yahoo.co.uk>
2018-09-11 12:11:21 +0100

    Removing unused import from Footer component

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import ContactInstructions from '../contact/ContactInstructions'
 import Ul, { Li } from '../layout/Ul'
 
 import {
