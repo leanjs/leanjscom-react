fbd250ec0097635424e06d32ce2178175f92114c
alexlbr <alex.lobera@gmail.com>
2020-05-18 23:39:56 +0100

    removes commented out code

--- src/components/buttons/ContactButton.js
+++ src/components/buttons/ContactButton.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { LinkIconButton } from './IconButton'
 import { CHAT } from '../../resources/icons'
 
-const ContactButton = () => (
-  <LinkIconButton image={CHAT} text="Contact" scroll to="contact" />
+const ContactButton = props => (
+  <LinkIconButton image={CHAT} text="Contact" scroll to="contact" {...props} />
 )
 
 export default ContactButton
